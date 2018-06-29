dee02a0e54fa2bb9e396a3bda8f9390b9f6c8b77
Nitzan <nitzanbr@hotmail.com>
2018-06-29 16:03:09 +0300

    adding games router

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,16 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path')
-const MongoClient = require('mongodb').MongoClient;
-const { connectionUrl } = require('../appConfig')
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-	MongoClient.connect(connectionUrl, (err, client) => {
-		const collection = client.db("test").collection("devices");
-		// perform actions on the collection object
-		client.close();
-	});
+	req.send("server is running")
 });
 
 module.exports = router;
