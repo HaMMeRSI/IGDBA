6b3aa262d8a079f759379b492d2cb2179dc6b6a9
Nitzan <nitzanbr@hotmail.com>
2018-08-04 17:14:42 +0300

    adding logs

--- Server/wsClient.js
+++ Server/wsClient.js
@@ -2,9 +2,12 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ port: 3001 });
 
-wss.on('connection', (ws) => {
+wss.on('connection', (ws, req) => {
+	const ip = req.connection.remoteAddress;
+	console.log(`Client connected: ${ip}`)
+
 	ws.on('message', (message) => {
-		console.log('received: %s', message);
+		console.log(`Message recieved: ${message}`);
 
 		for (client of wss.clients) {
 			wss.clients.forEach((client) => {
