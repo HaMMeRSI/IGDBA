b6ae599e33f1909346aea792c01655320199fdae
HuMMeR <sagih95@gmail.com>
2018-08-08 19:35:09 +0300

    Reshimat kniyot

--- Client/src/components/main/mainCtrl.js
+++ Client/src/components/main/mainCtrl.js
@@ -1,6 +1,11 @@
 export default class AppCtrl {
 	constructor ($scope) {
 		this.url = 'https://github.com/HuMMeRSI/IGDBA';
+		var c = document.getElementById('myCanvas');
+		var ctx = c.getContext('2d');
+		ctx.font = '20px Arial';
+		ctx.fillStyle = 'white';
+		ctx.fillText('IGDBA', 10, 30);
 	}
 }
 IGDBA.controller('appCtrl', AppCtrl);
