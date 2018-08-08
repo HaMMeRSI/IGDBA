389d6c441452b5d31f50c2cefe2711145a6c65f3
HuMMeR <sagih95@gmail.com>
2018-08-08 20:53:52 +0300

    Search

--- Client/src/components/main/mainComp.js
+++ Client/src/components/main/mainComp.js
@@ -10,7 +10,21 @@ export default function () {
 			template: require('./mainComponent.html'),
 			controller: 'appCtrl',
 			controllerAs: 'appa',
-			replace: true
+			restrict: 'E',
+			replace: true,
+			scope: {},
+			link (scope, element, attrs, tabsCtrl) {
+				scope.filter = {};
+				scope.onSearchClick = ($e) => {
+					Object.keys(scope.filter).forEach(key => {
+						if (scope.filter[key] === '') {
+							delete scope.filter[key];
+						}
+					});
+
+					window.location.href = `#!games?filter=${JSON.stringify(scope.filter)}`;
+				};
+			}
 		};
 	});
 }
